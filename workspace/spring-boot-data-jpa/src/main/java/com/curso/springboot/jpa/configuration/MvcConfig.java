a74893ed45840c70fb53e21aa665ab88c293fd20
MNEMO\jf.jimenez <jf.jimenez@mnemo.com>
2018-12-14 11:25:31 +0100

    Inicio de multiidioma

--- workspace/spring-boot-data-jpa/src/main/java/com/curso/springboot/jpa/configuration/MvcConfig.java
+++ workspace/spring-boot-data-jpa/src/main/java/com/curso/springboot/jpa/configuration/MvcConfig.java
@@ -1,12 +1,18 @@
 package com.curso.springboot.jpa.configuration;
 
 
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 
 @Configuration
@@ -30,4 +36,31 @@ public class MvcConfig implements WebMvcConfigurer {
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	/**************************************
+	 * 
+	 * Beans para la internacionalización.
+	 * 
+	 * ***********************************/
+	@Bean
+	public LocaleResolver localeResolver() {
+		// El locale se guardará enla sesión http.
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(new Locale("es","ES"));
+		return localeResolver;
+	}
+	
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		return localeChangeInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor( localeChangeInterceptor() );
+	}
+	
+	
 }
