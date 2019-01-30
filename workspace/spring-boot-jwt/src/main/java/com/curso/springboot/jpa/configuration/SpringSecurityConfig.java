dce8315bb26f01a477cb49f8c2f463bd4c8296ea
MNEMO\jf.jimenez <jf.jimenez@mnemo.com>
2019-01-30 12:35:45 +0100

    Modificación del POM y de la configuración de Spring Security

--- workspace/spring-boot-jwt/src/main/java/com/curso/springboot/jpa/configuration/SpringSecurityConfig.java
+++ workspace/spring-boot-jwt/src/main/java/com/curso/springboot/jpa/configuration/SpringSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 //import org.springframework.security.core.userdetails.User;
 //import org.springframework.security.core.userdetails.User.UserBuilder;
@@ -87,7 +88,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		 .logoutSuccessUrl("/login?logout")
 		 .invalidateHttpSession(true)
 		 .deleteCookies("JSESSIONID")
-		 .and().exceptionHandling().accessDeniedPage("/error_403");
+		 .and().exceptionHandling().accessDeniedPage("/error_403")
+		 // deshabilitamos csrf. Csrf se usa normalmente con formularios y no con rest
+		 .and()
+		 .csrf().disable()
+		 // deshabilitamos el uso de sesiones porque vamos a usar JSON Web Token
+		 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		 
 	}
 }
