7a668f54e0ed3c01e77ddffebfb7f28f4f8ac543
MNEMO\jf.jimenez <jf.jimenez@mnemo.com>
2019-02-01 10:06:38 +0100

    Finalizado el filtro de autentificación

--- workspace/spring-boot-jwt/src/main/java/com/curso/springboot/jpa/auth/filters/JWTAuthenticationFilter.java
+++ workspace/spring-boot-jwt/src/main/java/com/curso/springboot/jpa/auth/filters/JWTAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package com.curso.springboot.jpa.auth.filters;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,15 +14,22 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.StringUtils;
 
+import com.curso.springboot.jpa.models.bean.UserBean;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -44,13 +53,19 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		
 		String username = obtainUsername(request);
 		String password = obtainPassword(request);
-
-		if (username == null) {
-			username = "";
-		}
-
-		if (password == null) {
-			password = "";
+		
+		// Si no vienen como request parameters, buscamos usuario y clave en el body de la petición
+		// como un formato json.
+		if (StringUtils.isEmpty(username) && StringUtils.isEmpty(password)) {
+			UserBean user = null;
+			try {
+				user = new ObjectMapper().readValue(request.getInputStream(), UserBean.class);
+				username = user.getUsername();
+				password = user.getPassword();
+			} catch (Exception e) {
+				logger.error(e.getLocalizedMessage(), e);
+				throw new AuthenticationServiceException(e.getLocalizedMessage(), e);
+			} 
 		}
 		
 		logger.info("Usuario : " + username + ", Password : " + password);
@@ -61,15 +76,42 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	}
 
 	@Override
+	protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException failed) throws IOException, ServletException {
+		
+		// Contruimos el body que vamos a mandar
+		Map<String, Object> body = new HashMap<>();
+		body.put("message", "Error de autentificación usuario y/o password incorrecto");
+		body.put("error", failed.getLocalizedMessage());
+		
+		response.addHeader("Content-Type", "application/json; charset=UTF-8");
+		response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+		response.setStatus(HttpStatus.UNAUTHORIZED.value());
+	}
+
+	@Override
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
 			Authentication authResult) throws IOException, ServletException {
 		
 		String userName = ((User)authResult.getPrincipal()).getUsername();
+		Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
+		
+		
 		SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+		Long now = System.currentTimeMillis();
+		
+		Claims claims = Jwts.claims();
+		claims.put("authorities", new ObjectMapper().writeValueAsString(roles));
+		claims.put("sub", userName);
+		claims.put("iat", new Date(now));
+		claims.put("exp", new Date(now + 4*60*60*1000L));
 		
 		String jwtToken = Jwts.builder()
 							.setSubject( userName )
 							.signWith(key)
+							.setIssuedAt(new Date(now))
+							.setExpiration(new Date(now + 4*60*60*1000L) ) // 4 horas
+							.setClaims(claims)
 							.compact();
 		
 		// Generamos los encabezados...
