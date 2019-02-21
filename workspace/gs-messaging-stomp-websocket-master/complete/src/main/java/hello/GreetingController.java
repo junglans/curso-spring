60c7ea18c1d1195cfa59dc8f73fff3a1cb813537
MNEMO\jf.jimenez <cderegistro@gmail.com>
2019-02-21 13:07:29 +0100

    Modificaciones

--- workspace/gs-messaging-stomp-websocket-master/complete/src/main/java/hello/GreetingController.java
+++ workspace/gs-messaging-stomp-websocket-master/complete/src/main/java/hello/GreetingController.java
@@ -1,19 +1,35 @@
 package hello;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.HtmlUtils;
 
-@Controller
+@RestController
 public class GreetingController {
-
+	@Autowired
+	private SimpMessagingTemplate template;
+	
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
         Thread.sleep(1000); // simulated delay
         return new Greeting(message.getId(), "Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+    
+    @RequestMapping(value="/hello2", method=RequestMethod.POST)
+    public Greeting greeting2(@RequestBody HelloMessage message, HttpServletRequest request) throws Exception {
+    	 Greeting greeting =  new Greeting(message.getId(), "¡¡Hola, " + HtmlUtils.htmlEscape(message.getName()) + "!!");
+    	 this.template.convertAndSend("/topic/greetings", greeting);
+    	 return greeting;
     }
 
 }
