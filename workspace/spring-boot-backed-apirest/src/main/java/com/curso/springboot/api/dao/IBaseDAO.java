ea5890264a85424e398e2d88839d9712aa88a71b
MNEMO\jf.jimenez <cderegistro@gmail.com>
2019-03-05 09:44:13 +0100

    Refactorización de código

---
+++ workspace/spring-boot-backed-apirest/src/main/java/com/curso/springboot/api/dao/IBaseDAO.java
@@ -0,0 +1,104 @@
+package com.curso.springboot.api.dao;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import com.curso.springboot.api.builders.ExpressionBuilder;
+import com.curso.springboot.api.builders.ExpressionBuilderFactory;
+import com.curso.springboot.api.utils.CollectionUtils;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.EntityPathBase;
+import com.querydsl.core.types.dsl.ListPath;
+import com.querydsl.core.types.dsl.SimpleExpression;
+
+@NoRepositoryBean
+public interface IBaseDAO<T, Q> extends JpaRepository<T, Q>, QuerydslPredicateExecutor<T> {
+
+	default List<T> findAll(EntityPathBase<?> entityPathBase, FilterBy... filters) throws Exception {
+
+		BooleanExpression finalPredicate = null;
+		for (FilterBy filter : filters) {
+
+			Object fieldPath = getFieldPath(filter.getAttrName(), entityPathBase);
+			// Recuperamos el método que implementa la operación indicada en el filtro. Nos
+			// interesan los métodos que tienen
+			// parámetros de tipo Expression<?>.
+			Method method = getMethod(filter, fieldPath);
+			ExpressionBuilder<?> builder = ExpressionBuilderFactory.getExpressionBuilder(filter, method);
+
+			// Aqui el resultado de la llmada es un objeto de tipo Expression o Expression[]
+			Object val = builder.build(filter);
+			BooleanExpression predicate = null;
+			switch (method.getParameterTypes().length) {
+			case 1:
+				predicate = (BooleanExpression) method.invoke(fieldPath, val);
+				finalPredicate = (finalPredicate == null) ? predicate : finalPredicate.and(predicate);
+				break;
+			case 2:
+				Object[] values = (Object[]) val;
+				predicate = (BooleanExpression) method.invoke(fieldPath, values[0], values[1]);
+				finalPredicate = (finalPredicate == null) ? predicate : finalPredicate.and(predicate);
+				break;
+			}
+		}
+
+		return CollectionUtils.iterableToCollection(findAll(finalPredicate));
+	}
+
+	default Object getFieldPath(String[] attrName, EntityPathBase<?> entityPathBase) throws Exception {
+		// Utilizando reflexión recuperamos el campo de búsqueda a partir del nombre del
+		// campo que viene en el filtro.
+		// El nombre del campo puede ser una lista de nombres separada por ".".
+
+		// Buscamos el primer nivel
+		Field filterField = entityPathBase.getClass().getField(attrName[0]);
+
+		Object fieldPath = ((Object) filterField.get(entityPathBase));
+		if (attrName.length > 1) {
+			if (fieldPath.getClass().isAssignableFrom(ListPath.class)) {
+
+				SimpleExpression<?> qentity = ((ListPath<?, ?>) fieldPath).any();
+				fieldPath = getFieldPath(Arrays.copyOfRange(attrName, 1, attrName.length),(EntityPathBase<?>) qentity);
+
+			} else {
+				fieldPath = getFieldPath(Arrays.copyOfRange(attrName, 1, attrName.length),(EntityPathBase<?>) fieldPath);
+			}
+		}
+		return fieldPath;
+	}
+
+	default Method getMethod(FilterBy filter, Object fieldPath) throws Exception {
+		Method method = null;
+		for (Method m : fieldPath.getClass().getMethods()) {
+//			System.out.println(m.getName());
+			if (m.getName().equals(filter.getAttrOperation())) {
+				switch (m.getParameterTypes().length) {
+				case 1: // eq, ne, ge, gt, in, not in
+					if ((m.getParameterTypes()[0].equals(com.querydsl.core.types.Expression.class)
+							|| m.getParameterTypes()[0].equals(com.querydsl.core.types.Expression[].class))) {
+						method = m;
+					}
+					break;
+				case 2: // between
+					if ((m.getParameterTypes()[0].equals(com.querydsl.core.types.Expression.class)
+							&& m.getParameterTypes()[1].equals(com.querydsl.core.types.Expression.class))) {
+						method = m;
+					}
+					break;
+				default:
+					throw new Exception("Wrong number of parameters :" + m.getParameterTypes().length);
+				}
+			}
+			if (method != null) {
+				break;
+			}
+		}
+		return method;
+	}
+}
--- workspace/spring-boot-backed-apirest/src/main/java/com/curso/springboot/api/services/ClientServiceImpl.java
+++ workspace/spring-boot-backed-apirest/src/main/java/com/curso/springboot/api/services/ClientServiceImpl.java
@@ -2,8 +2,6 @@ package com.curso.springboot.api.services;
 
 import java.util.List;
 
- 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,36 +14,36 @@ import com.curso.springboot.api.entity.ClientEntity;
 public class ClientServiceImpl implements IClientService {
 
 	@Autowired
-	private IClientDAO dao;
+	private IClientDAO clientDao;
 	
 	@Override
 	@Transactional(readOnly= true)
 	public List<ClientEntity> findAll() throws Exception {
-		return (List<ClientEntity>) dao.findAll();
+		return (List<ClientEntity>) clientDao.findAll();
 	}
 	
 	@Override
 	@Transactional(readOnly= true)
 	public ClientEntity findById(Long id) throws Exception {
-		return dao.findById(id).get();
+		return clientDao.findById(id).get();
 	}
 
 	@Override
 	@Transactional
 	public ClientEntity save(ClientEntity client) throws Exception {
-		return dao.save(client);
+		return clientDao.save(client);
 	}
 
 	@Override
 	@Transactional
 	public void delete(ClientEntity entity) throws Exception {
-		dao.delete(entity);
+		clientDao.delete(entity);
 	}
 
 	@Override
 	@Transactional(readOnly= true)
 	public List<ClientEntity> findAll(FilterBy...filter) throws Exception {
-		return (List<ClientEntity>) dao.findAll(filter);
+		return (List<ClientEntity>) clientDao.findAll(filter);
 	}
 
 }
